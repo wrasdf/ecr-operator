f03d8f88aca4bdd0e99e8479909154ebf3f6fee5
wrasdf <452784+wrasdf@users.noreply.github.com>
2021-02-02 15:15:58 +1100

    update shell-hook

--- hooks/shell-hook.sh
+++ hooks/shell-hook.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
 
 if [[ $1 == "--config" ]] ; then
-  echo '{"configVersion":"v1", "onStartup": 1}'
+  cat <<EOF
+configVersion: v1
+kubernetes:
+  - apiVersion: v1alpha1
+    kind: ECR
+    executeHookOnEvent:
+      - Added
+      - Deleted
+EOF
 else
-  echo "OnStartup shell hook"
+  type=$(jq -r '.[0].type' ${BINDING_CONTEXT_PATH})
+  if [[ $type == "Synchronization" ]] ; then
+    : handle existing objects
+    : jq '.[0].objects | ... '
+  fi
+
+  if [[ $type == "Event" ]] ; then
+    name=$(jq -r '.[0].object.metadata.name' ${BINDING_CONTEXT_PATH})
+    kind=$(jq -r '.[0].object.kind' ${BINDING_CONTEXT_PATH})
+    echo "${kind}/${name} object is added"
+  fi
 fi
